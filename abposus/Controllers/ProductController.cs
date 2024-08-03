4432997a82824f57ade3abcd6682126fe912f290
Josue Coreas <josuepaiz487@gmail.com>
2024-08-03 15:34:52 -0600

    Showing products list

---
+++ abposus/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using abposus.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace abposus.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+    }
+}
